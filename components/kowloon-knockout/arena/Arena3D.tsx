ce3809685d3072c1f865a7268cc37028eb75a196
ka1kqi <arthur.x.du@gmail.com>
2026-06-25 20:15:09 -0700

    feat(kowloon): IBL environment + noir key/neon-fill lighting

--- components/kowloon-knockout/arena/Arena3D.tsx
+++ components/kowloon-knockout/arena/Arena3D.tsx
@@ -4,6 +4,7 @@ import { useRef, type MutableRefObject } from 'react';
 import { useFrame } from '@react-three/fiber';
 import type { GameSession, RenderFighter } from '@/lib/kowloon-knockout/net/session';
 import Environment from './Environment';
+import Lighting from './Lighting';
 import StickFighter from './StickFighter';
 import CameraRig from './CameraRig';
 import Fx from './Fx';
@@ -31,22 +32,7 @@ export default function Arena3D({ session, seatIds }: { session: GameSession; se
             <color attach="background" args={['#070010']} />
             <fog attach="fog" args={['#0a0118', 18, 60]} />
 
-            <ambientLight intensity={0.55} color="#5a4a7a" />
-            <hemisphereLight args={['#ff5588', '#1a0a2a', 0.5]} />
-            <directionalLight
-                position={[6, 14, 8]}
-                intensity={1.1}
-                color="#ffe0c0"
-                castShadow
-                shadow-mapSize-width={1024}
-                shadow-mapSize-height={1024}
-                shadow-camera-left={-12}
-                shadow-camera-right={12}
-                shadow-camera-top={12}
-                shadow-camera-bottom={-12}
-            />
-            <pointLight position={[-8, 6, -4]} intensity={60} color="#33ccff" distance={40} />
-            <pointLight position={[8, 6, 4]} intensity={60} color="#ff3366" distance={40} />
+            <Lighting />
 
             <FrameSync session={session} framesRef={framesRef} shakeRef={shakeRef} />
             <CameraRig framesRef={framesRef} shakeRef={shakeRef} />
